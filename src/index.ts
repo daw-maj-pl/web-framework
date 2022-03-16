ab284bf01a19d734994a871fcf1f37463cc0a6b0
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-16 19:24:49 +0100

    Implement set method in User class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 30 });
 
+user.set({ name: 'Mark', age: 35 });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
