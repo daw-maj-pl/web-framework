d22c033520c6351c8e9937ea7c80c5c0891f6ddb
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-17 08:44:23 +0100

    Make the name and age properties optional

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 30 });
 
-user.set({ name: 'Mark', age: 35 });
+user.set({ name: 'Mark' });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
