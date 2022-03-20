8026ec8bb9f1e76bac10b7f5c9adf89d7212b607
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-20 10:55:11 +0100

    Extract eventing logic into separate clsss; (Refactor User to use composition)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'James', age: 28 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('change');
+});
+
+user.events.trigger('change');
