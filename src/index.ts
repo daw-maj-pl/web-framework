db094d684411c88df005157611eb96be162f0297
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-24 14:00:39 +0100

    Reimplement get method using accessor

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'James', age: 28 });
 
+console.log(user.get('name'));
+
 user.on('change', () => {
   console.log('change');
 });
--- src/models/User.ts
+++ src/models/User.ts
@@ -26,4 +26,8 @@ export class User {
   get trigger() {
     return this.events.trigger;
   }
+
+  get get() {
+    return this.attributes.get;
+  }
 }
