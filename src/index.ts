abcd23864945c987cfe1f9030a8efcb9aa0c3972
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-24 16:13:56 +0100

    Reimplement fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'James', age: 28 });
-
-console.log(user.get('name'));
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-  console.log('User was changed, we probably need to update some HTML');
+  console.log(user);
 });
 
-user.set({ name: 'New name' });
+user.fetch();
