4ab198722b4c75ecc0e59b0f007d9c8f86bee2f4
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-25 13:22:06 +0100

    Test save method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: 'Olivia', age: 25 });
 
-user.on('change', () => {
+user.on('save', () => {
   console.log(user);
 });
 
-user.fetch();
+user.save();
