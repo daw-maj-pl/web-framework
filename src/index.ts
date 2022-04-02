6b7d498cb20f14e0b6103d25026d9055639cf4ba
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-04-02 22:16:35 +0200

    Add model properties into template

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Alex', age: 33 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.render();
